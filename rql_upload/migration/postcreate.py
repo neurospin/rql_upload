4aee86a67b4aa358b9185ea4f2c114957d2f4171
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2016-03-11 15:41:13 +0100

    Copyright

--- rql_upload/migration/postcreate.py
+++ rql_upload/migration/postcreate.py
@@ -1,19 +1,11 @@
 # -*- coding: utf-8 -*-
-# copyright 2014 NSAp, all rights reserved.
-# contact http://www.logilab.fr -- mailto:contact@logilab.fr
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 2.1 of the License, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with this program. If not, see <http://www.gnu.org/licenses/>.
+##########################################################################
+# NSAp - Copyright (C) CEA, 2014
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
 
 """cubicweb-rql-upload postcreate script, executed at instance creation time or when
 the cube is added to an existing instance.
@@ -23,4 +15,3 @@ You could setup site properties or a workflow here for example.
 
 # Example of site property change
 #set_property('ui.site-title', "<sitename>")
-
